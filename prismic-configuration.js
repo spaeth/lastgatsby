2de9a2e12b86870efbd17a1b88bbc8e1ef64f059
spaeth <tho.spaeth@googlemail.com>
2021-01-27 22:59:41 +0100

    try 2

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -3,8 +3,8 @@ module.exports = {
   prismicRepo: 'spaeth',
 
   // Release ID to build (requires access)
-  releaseID: '',
+  releaseID: 'YBHZVhAAAEMGOCGe',
 
   // access token to access the release
-  accessToken: '',
+  accessToken: 'MC5ZQkhYN2hBQUFBQUlPQnRz.Uu-_vT7vv70RLu-_ve-_vTnvv73vv73vv70wD--_ve-_ve-_vQzvv70F77-9eu-_vS0n77-977-977-9Bu-_ve-_ve-_vQ',
 }
